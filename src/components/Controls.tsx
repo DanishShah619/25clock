5e131f5cc53dccf606a3c4c316385ee4914e8058
Danish Shah <shanildanshah@gmail.com>
2025-03-13 17:17:02 +0530

    minor change

--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -7,7 +7,7 @@ interface ControlsProps {
   onStartStop: () => void; 
 }
 
-const Controls: React.FC<ControlsProps> = ({ isRunning, onReset,onStartStop }) => {
+const Controls: React.FC<ControlsProps> = ({ isRunning: _isRunning, onReset,onStartStop }) => {
   return (
     <div className="flex gap-4">
       <button id="start_stop" className="p-2 bg-green-500 rounded"onClick={onStartStop}>Start / Stop</button>
